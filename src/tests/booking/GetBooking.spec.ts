c3c40d9c97c56966c3b33b7f930decfcc12a40aa
Damian Pereira <damianpereira86@gmail.com>
2024-12-07 10:41:09 -0300

    Update tests

--- src/tests/booking/GetBooking.spec.ts
+++ src/tests/booking/GetBooking.spec.ts
@@ -1,15 +1,18 @@
 import { BookingModel } from "../../models/request/BookingModel.js";
 import { BookingResponse } from "../../models/responses/BookingResponse.js";
 import { BookingService } from "../../models/services/BookingService.js";
+import { Response } from "../../models/responses/Response.js";
 import chai from "chai";
 
 chai.should();
 
 describe("Get Booking", () => {
   const bookingService = new BookingService();
+  let bookingId: number;
+  let createdBooking: Response<BookingResponse>;
 
-  it("@Smoke - Get Booking successfully - 200", async () => {
-    const booking = await bookingService.addBooking<BookingResponse>({
+  before(async () => {
+    createdBooking = await bookingService.addBooking<BookingResponse>({
       firstname: "Damian",
       lastname: "Pereira",
       totalprice: 1000,
@@ -21,34 +24,26 @@ describe("Get Booking", () => {
       additionalneeds: "Breakfast",
     });
 
-    const bookingId = booking.data.bookingid;
+    bookingId = createdBooking.data.bookingid;
+  });
 
+  it("@Smoke - Get Booking successfully - 200", async () => {
     const response = await bookingService.getBooking<BookingModel>(bookingId);
     response.status.should.equal(200, JSON.stringify(response.data));
-    response.data.firstname?.should.equal(booking.data.booking.firstname);
-    response.data.lastname?.should.equal(booking.data.booking.lastname);
-    response.data.totalprice?.should.equal(booking.data.booking.totalprice);
+    response.data.firstname?.should.equal(createdBooking.data.booking.firstname);
+    response.data.lastname?.should.equal(createdBooking.data.booking.lastname);
+    response.data.totalprice?.should.equal(createdBooking.data.booking.totalprice);
     response.data.depositpaid?.should.be.true;
-    response.data.bookingdates?.checkin?.should.equal(booking.data.booking.bookingdates?.checkin);
-    response.data.bookingdates?.checkout?.should.equal(booking.data.booking.bookingdates?.checkout);
-    response.data.additionalneeds?.should.equal(booking.data.booking.additionalneeds);
+    response.data.bookingdates?.checkin?.should.equal(
+      createdBooking.data.booking.bookingdates?.checkin,
+    );
+    response.data.bookingdates?.checkout?.should.equal(
+      createdBooking.data.booking.bookingdates?.checkout,
+    );
+    response.data.additionalneeds?.should.equal(createdBooking.data.booking.additionalneeds);
   });
 
   it("@Regression - Get Booking successfully - Response time < 1000 ms", async () => {
-    const booking = await bookingService.addBooking<BookingResponse>({
-      firstname: "Damian",
-      lastname: "Pereira",
-      totalprice: 1000,
-      depositpaid: true,
-      bookingdates: {
-        checkin: "2024-01-01",
-        checkout: "2024-02-01",
-      },
-      additionalneeds: "Breakfast",
-    });
-
-    const bookingId = booking.data.bookingid;
-
     const response = await bookingService.getBooking<BookingModel>(bookingId);
     response.responseTime.should.be.lessThan(1000);
   });
